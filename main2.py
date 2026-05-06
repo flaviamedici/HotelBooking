1a229a6d0c2e79edefcc8b616f63b91ff834face
flaviamedici <flaviaomedici@gmail.com>
2026-05-06 13:57:06 -0700

    main2.py file created with some simplified script

---
+++ main2.py
@@ -0,0 +1,57 @@
+from collections import namedtuple
+
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+
+
+class Hotel:
+    watermark = "The Real Estate Company"
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] ==self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Checks if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+    #class method
+    @classmethod
+    def get_hotel_count(cls, data):
+        return len(data)
+
+class Reservation:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+
+    def generate(self):
+        content = (f"""
+            Thank you for your reservation!
+            Here are the booking data:
+            Name: {self.customer_name}
+            Hotel name: {self.hotel.name}
+        """)
+        return content
+
+hotel1 = Hotel(hotel_id=188)
+hotel2 = Hotel(hotel_id=134)
+
+print(hotel1.available())
+
+print(hotel1.name)
+
+print(hotel2.name)
+
+print(Hotel.get_hotel_count(data=df))
